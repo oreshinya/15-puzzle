e80b17a0df1d6c12212ef93ddc45e4ea27fb72b5
abab <ababup1192@gmail.com>
2017-12-14 17:46:56 +0900

    Add isClear test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -7,7 +7,7 @@ import Dict
 
 -- Test target modules
 
-import Puzzle exposing (Piece(..), move)
+import Puzzle exposing (Piece(..), move, isClear)
 
 
 all : Test
@@ -95,4 +95,30 @@ all =
                     , ( ( 2, 3 ), Num 14 )
                     , ( ( 3, 3 ), Num 15 )
                     ]
+        , "final move"
+            => isClear
+                (move ( 2, 3 )
+                    ( 3, 3 )
+                    (Num 15)
+                    (Dict.fromList
+                        [ ( ( 0, 0 ), Num 1 )
+                        , ( ( 1, 0 ), Num 2 )
+                        , ( ( 2, 0 ), Num 3 )
+                        , ( ( 3, 0 ), Num 4 )
+                        , ( ( 0, 1 ), Num 5 )
+                        , ( ( 1, 1 ), Num 6 )
+                        , ( ( 2, 1 ), Num 7 )
+                        , ( ( 3, 1 ), Num 8 )
+                        , ( ( 0, 2 ), Num 9 )
+                        , ( ( 1, 2 ), Num 10 )
+                        , ( ( 2, 2 ), Num 11 )
+                        , ( ( 3, 2 ), Num 12 )
+                        , ( ( 0, 3 ), Num 13 )
+                        , ( ( 1, 3 ), Num 14 )
+                        , ( ( 2, 3 ), Blank )
+                        , ( ( 3, 3 ), Num 15 )
+                        ]
+                    )
+                )
+            === True
         ]
