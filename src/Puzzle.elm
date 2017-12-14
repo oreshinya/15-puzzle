2d8612de32f78322e8a2ae8ea8f228f45b855a24
abab <ababup1192@gmail.com>
2017-12-14 15:10:37 +0900

    Add move function

--- src/Puzzle.elm
+++ src/Puzzle.elm
@@ -1,10 +1,11 @@
 module Puzzle exposing (..)
 
 import Dict exposing (Dict)
+import DictUtil exposing (swap)
 
 
 type alias XY =
-    { x : Int, y : Int }
+    ( Int, Int )
 
 
 type Piece
@@ -14,3 +15,26 @@ type Piece
 
 type alias Board =
     Dict XY Piece
+
+
+addXY : XY -> XY -> XY
+addXY ( x1, y1 ) ( x2, y2 ) =
+    ( x1 + x2, y1 + y2 )
+
+
+dirs : List XY
+dirs =
+    [ ( 0, -1 ), ( 1, 0 ), ( 0, -1 ), ( -1, 0 ) ]
+
+
+isMove : XY -> XY -> Bool
+isMove bxy xy =
+    List.member xy <| List.map (\xy_ -> addXY bxy xy_) dirs
+
+
+move : XY -> XY -> Piece -> Board -> Board
+move bxy xy piece board =
+    if isMove bxy xy then
+        swap bxy Blank xy piece board
+    else
+        board
