c6312b726f5f1aa2bd65ea421ad7749165f41840
abab <ababup1192@gmail.com>
2017-12-14 13:57:21 +0900

    Add Board as a data structure

--- src/Puzzle.elm
+++ src/Puzzle.elm
@@ -1,5 +1,16 @@
 module Puzzle exposing (..)
 
+import Dict exposing (Dict)
+
 
 type alias XY =
     { x : Int, y : Int }
+
+
+type Piece
+    = Num Int
+    | Blank
+
+
+type alias Board =
+    Dict XY Piece
